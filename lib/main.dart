8475f12cf096a35d7218bf47e853f50aecd30788
Saif  Noor <saif.noor@northsouth.edu>
2022-03-07 11:59:22 +0600

    Demo commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'register.dart';
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'register': (context) => MyRegister(),
+      'login': (context) => MyLogin(),
+    },
+  ));
+}
